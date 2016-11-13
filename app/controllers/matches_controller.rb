79fe789eba65daaaeb7cd5e00ef6cb99dc7db052
cyi106 <cyi106@users.noreply.github.com>
2016-11-12 19:18:00 -0600

    Sort relevance score in controller v0

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,54 +1,63 @@
 class MatchesController  < ApplicationController
 
-def search_form
-end
+  def search_form
+  end
 
 
-def add_row
-r = Request.new
-r.user_id = current_user.id
-r.genreselect = params[:genreselect]
-r.skillsearch_production = params[:skillsearch_production]
-r.skillsearch_vocals = params[:skillsearch_vocals]
-r.skillsearch_liveinstrumentation = params[:skillsearch_liveinstrumentation]
-r.skillsearch_mixingandmastering = params[:skillsearch_mixingandmastering]
-r.search_text = params[:search_text]
-r.save
+  def add_row
+    r = Request.new
+    r.user_id = current_user.id
+    r.genreselect = params[:genreselect]
+    r.skillsearch_production = params[:skillsearch_production]
+    r.skillsearch_vocals = params[:skillsearch_vocals]
+    r.skillsearch_liveinstrumentation = params[:skillsearch_liveinstrumentation]
+    r.skillsearch_mixingandmastering = params[:skillsearch_mixingandmastering]
+    r.search_text = params[:search_text]
+    r.save
 
-  redirect_to("/search/results/production/#{r.id}")
-end
+    redirect_to("/search/results/production/#{r.id}")
+  end
 
 
-def results_production
-    @request = Request.find(params[:id])
-    
-    @results = User.where(vocals:true)
-
-end
-
-def results_vocals
+  def results_production
     @request = Request.find(params[:id])
 
+    if genre1 = "Hip Hop"
+      @genrepoints = 10
+    elsif
+      @genrepoints = 0
+    end
+
+
+  @results =  User.where(production:true).sort_by(&@genrepoints)
+  #@results =  User.where(production:true).sort_by(&:genrepoints).reverse
+
+
+  end
+
+  def results_vocals
+    @request = Request.find(params[:id])
+
     @results = User.where(vocals:true)
 
 
-end
+  end
 
-def results_instruments
+  def results_instruments
     @request = Request.find(params[:id])
 
     @results = User.where(liveinstrumentation:true)
 
 
-end
+  end
 
-def results_mixandmaster
+  def results_mixandmaster
     @request = Request.find(params[:id])
 
     @results = User.where(mixingandmastering:true)
 
 
-end
+  end
 
 
 end
