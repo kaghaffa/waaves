7a4c6f4cb4d4c1dd96a02b2da192631735acd6f1
cyi106 <cyi106@users.noreply.github.com>
2017-01-04 13:53:39 -0600

    Database re-seeded

--- db/migrate/20170103142856_remove_ur_ls_from_profile.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUrLsFromProfile < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :soundcloudurl
-    remove_column :users, :youtubeurl
-    remove_column :users, :bandcampurl
-    remove_column :users, :googledriveurl
-    remove_column :users, :otherurl
-  end
-end
